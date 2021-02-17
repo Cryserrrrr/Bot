743cb48782dd1a485e89cf5e46b9be51f2d5f372
Cryserrrrr <66973532+Cryserrrrr@users.noreply.github.com>
2021-02-17 16:23:22 +0100

    Delete musique.js

--- Commandes/musique.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js')
-
-module.exports.run = async (client, message, args) => {
-        message.reply("Voici la meilleur musique : https://open.spotify.com/track/6K4t31amVTZDgR3sKmwUJJ?si=IpIOMoMPR9eSXtLXMjZ1bw")
-    }
-
-    module.exports.help = {
-    name: 'musique',
-}
